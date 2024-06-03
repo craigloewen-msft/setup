38d32241428a25f33c2dc50641771e3193b21dd9
Craig Loewen <crloewen@microsoft.com>
2024-06-03 16:13:36 -0400

    Updated to personalize

--- boot.ps1
+++ boot.ps1
@@ -52,15 +52,14 @@ else {
 
 $isAdmin = ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)
 
-$dscUri = "https://raw.githubusercontent.com/crutkas/setup/main/"
-$dscNonAdmin = "crutkas.nonAdmin.dsc.yml";
-$dscAdmin = "crutkas.dev.dsc.yml";
-$dscOffice = "crutkas.office.dsc.yml";
-$dscPowerToysEnterprise = "Z:\source\powertoys\.configurations\configuration.vsEnterprise.dsc.yaml";
+$dscUri = "https://raw.githubusercontent.com/crloewen/setup/main/"
+$dscNonAdmin = "crloewen.nonAdmin.dsc.yml";
+$dscDev = "crloewen.dev.dsc.yml";
+$dscOffice = "crloewen.office.dsc.yml";
 
 $dscOfficeUri = $dscUri + $dscOffice;
 $dscNonAdminUri = $dscUri + $dscNonAdmin 
-$dscAdminUri = $dscUri + $dscAdmin
+$dscDevUri = $dscUri + $dscDev
 
 # amazing, we can now run WinGet get fun stuff
 if (!$isAdmin) {
@@ -83,56 +82,43 @@ else {
     # ---------------
     # Installing office workload
     Write-Host "Start: Office install"
-    New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\' -Force
-    New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\' -Name 'DefaultProfile' -Value "OutlookAuto" -PropertyType String -Force
+   #  New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\' -Force
+   #  New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\' -Name 'DefaultProfile' -Value "OutlookAuto" -PropertyType String -Force
 
-    New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\OutlookAuto' -Force
-    New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\OutlookAuto' -Name 'Default' -Value "" -PropertyType String -Force
+   #  New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\OutlookAuto' -Force
+   #  New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\OutlookAuto' -Name 'Default' -Value "" -PropertyType String -Force
 
 
-    New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\AutoDiscover' -Force
-    New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\AutoDiscover' -Name 'ZeroConfigExchange' -Value "1" -PropertyType DWORD -Force
+   #  New-Item -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\AutoDiscover' -Force
+   #  New-ItemProperty -Path 'HKCU:\Software\Microsoft\Office\16.0\Outlook\AutoDiscover' -Name 'ZeroConfigExchange' -Value "1" -PropertyType DWORD -Force
 
-    gpupdate /force
+   #  gpupdate /force
 
     Invoke-WebRequest -Uri $dscOfficeUri -OutFile $dscOffice 
     winget configuration -f $dscOffice 
     Remove-Item $dscOffice -verbose
-    Start-Process outlook.exe
     Start-Process ms-teams.exe
     Write-Host "Done: Office install"
     # Ending office workload
     # ---------------
-   # Forcing Windows Update -- goal is move to dsc
-   Write-Host "Start: Windows Update"
-    $UpdateCollection = New-Object -ComObject Microsoft.Update.UpdateColl
-    $Searcher = New-Object -ComObject Microsoft.Update.Searcher
-    $Session = New-Object -ComObject Microsoft.Update.Session
-    $Installer = New-Object -ComObject Microsoft.Update.Installer
- 
-    $Searcher.ServerSelection = 2
- 
-    $Result = $Searcher.Search("IsInstalled=0 and IsHidden=0")
- 
-    $Downloader = $Session.CreateUpdateDownloader()
-    $Downloader.Updates = $Result.Updates
-    $Downloader.Download()
- 
-    $Installer.Updates = $Result.Updates
-    $Installer.Install()
-    Write-Host "Done: Windows Update"
-    # Forcing Windows Update complete 
 
     # Staring dev workload
     Write-Host "Start: Dev flows install"
-    Invoke-WebRequest -Uri $dscAdminUri -OutFile $dscAdmin 
-    winget configuration -f $dscAdmin 
-
-    Write-Host "Start: PowerToys dsc install"
-    winget configuration -f $dscPowerToysEnterprise # no cleanup needed as this is intentionally local
+    Invoke-WebRequest -Uri $dscDevUri -OutFile $dscDev 
+    winget configuration -f $dscDev 
    
     # clean up, Clean up, everyone wants to clean up
-    Remove-Item $dscAdmin -verbose
+    Remove-Item $dscDev -verbose
     Write-Host "Done: Dev flows install"
     # ending dev workload
+
+    Write-Host "Start: vs code extensions install"
+    # Install VS Code extension
+    code --install-extension ms-vscode-remote.remote-wsl
+    code --install-extension ms-vscode-remote.devcontainer
+    code --install-extension mhutchie.git-graph
+    code --install-extension GitHub.copilot
+    code --install-extension vscodevim.vim
+
+    Write-Host "Done: vs code extensions install"
 }
